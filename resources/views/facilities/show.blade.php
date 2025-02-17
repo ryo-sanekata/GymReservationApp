3fdc48938ed4dc123a235a6300f4dbf22b20410f
ryo-sanekata <ryosnkt7190@gmail.com>
2025-02-17 10:51:08 +0000

    changed display of reserve button when not logged in

--- resources/views/facilities/show.blade.php
+++ resources/views/facilities/show.blade.php
@@ -7,10 +7,12 @@
     <p><strong>説明:</strong> {{ $facility->description }}</p>
     <p><strong>1時間あたりの料金:</strong> ¥{{ $facility->price_per_hour }}</p>
     <p><strong>カテゴリー:</strong> {{ $facility->category }}</p>
+    @if(auth()->check() && !auth()->user()->admin)
+    <a href="{{ route('reservations.create', ['facility' => $facility->id]) }}" class="btn btn-primary">予約する</a>
+    @elseif(!auth()->check())
+        <a href="{{ route('login', ['redirect' => route('reservations.create', ['facility' => $facility->id])]) }}" class="btn btn-primary">ログインして予約</a>
+    @endif
 
-    <a href="{{ route('reservations.create', ['facility' => $facility->id]) }}" class="btn btn-primary">
-        予約する
-    </a>
     <a href="{{ route('facilities.index') }}" class="btn btn-secondary">トップへ戻る</a>
 </div>
 @endsection
